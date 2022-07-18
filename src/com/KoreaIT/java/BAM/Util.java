25b42b086b6eebbd15a6906653f895c4f38d4204
byeonghunki <orangeys1000@gmail.com>
2022-07-18 13:26:45 +0900

    게시물작성시날짜및시간저장

---
+++ src/com/KoreaIT/java/BAM/Util.java
@@ -0,0 +1,18 @@
+package com.KoreaIT.java.BAM;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Util {
+	public static String getNowDateStr() {
+
+		SimpleDateFormat sdf1 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+		Date now = new Date();
+
+		return sdf1.format(now);
+	}
+}
+	
+
+

781a6d076d9c894b3fdf11a32de7fca3ae606b22
byeonghunki <orangeys1000@gmail.com>
2022-07-18 11:26:47 +0900

    게시물 삭제 기능 구현

--- src/com/KoreaIT/java/BAM/Main.java
+++ src/com/KoreaIT/java/BAM/Main.java
@@ -35,11 +35,75 @@ public class Main {
 				String body = sc.nextLine();
 
 				Article article = new Article(id, title, body);
+				articles.add(article);
 
 				System.out.printf("%d번 글이 생성되었습니다\n", id);
 
 			} else if (cmd.equals("article list")) {
-				System.out.println("게시물이 없습니다");
+				if (articles.size() == 0) {
+					System.out.println("게시물이 없습니다");
+					continue;
+				}
+				System.out.println("번호    |   제목");
+				for (int i = articles.size() - 1; i >= 0; i--) {
+					Article article = articles.get(i);
+
+					System.out.printf("%d	|	%s\n", article.id, article.title);
+				}
+
+			} else if (cmd.startsWith("article detail ")) {
+
+				String[] cmdBits = cmd.split(" ");
+
+				int id = Integer.parseInt(cmdBits[2]);
+
+				Article foundArticle = null;
+
+				for (int i = 0; i < articles.size(); i++) {
+					Article article = articles.get(i);
+
+					if (article.id == id) {
+						foundArticle = article;
+
+						break;
+					}
+				}
+
+				if (foundArticle == null) {
+					System.out.printf("%d번 게시물은 없습니다.\n", id);
+					continue;
+				} else {
+
+					System.out.printf("번호 : %d\n",foundArticle.id);
+					System.out.printf("날짜 : %d\n",foundArticle.id);
+					System.out.printf("제목 : %s\n" , foundArticle.title);
+					System.out.printf("내용 : %s\n" ,foundArticle.body);
+
+				}
+			} else if (cmd.startsWith("article delete ")) {
+
+				String[] cmdBits = cmd.split(" ");
+
+				int id = Integer.parseInt(cmdBits[2]);
+
+				int foundIndex = -1;
+
+				for (int i = 0; i < articles.size(); i++) {
+					Article article = articles.get(i);
+
+					if (article.id == id) {
+						foundIndex = i;
+						break;
+					}
+				}
+
+				if (foundIndex == -1) {
+					System.out.printf("%d번 게시물은 없습니다.\n", id);
+					continue;
+				}
+				articles.remove(foundIndex);
+				System.out.printf("%d번 게시물을 삭제했습니다.\n",id);
+				
 			} else {
 				System.out.println("존재하지 않는 명령어입니다");
 			}
